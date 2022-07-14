390106eea681e41b19e0fb48b23f871c891011a2
Joel <joelgaray2020@gmail.com>
2022-07-14 15:49:55 -0500

    FIX

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import { useContext, useEffect } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { DataContext } from "./context/DataContext";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
+
+  const { isLogged, setIsLogged } = useContext(DataContext);
+
+
+
+  useEffect(() => {
+    (function (src) {
+      var a = document.createElement("script");
+      a.type = "text/javascript";
+      a.async = true;
+      a.src = src;
+      var b = document.getElementsByTagName("script")[0];
+      b.parentNode.insertBefore(a, b);
+    })("//sandbox.tinypass.com/xbuilder/experience/load?aid=PeVZORGJsu");
+
+    setTimeout(() => {
+      const tp = window.tp || [];
+      tp.push(["setCookieDomain", "project-dm.vercel.app"]);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    console.log('el usuario esta logueado?', isLogged);
+    setTimeout(() => {
+      setIsLogged(tp.pianoId.isUserValid());
+    }, 1000);
+  }, []);
+
+
   return (
     <>
       <Header />
